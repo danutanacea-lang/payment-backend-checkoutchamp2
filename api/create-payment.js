3b9dae6e034e3c359e878647cf07d9bd6a9944e7
danutanacea-lang <danuta.nacea@globalbrother.com>
2026-03-20 14:30:49 +0200

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -16,7 +16,7 @@ export default function handler(req, res) {
 
   const options = {
     hostname: 'ca4f1491abb67c33-GlobalBrother-checkout-live.adyenpayments.com',
-    path: '/v68/payments',
+path: '/checkout/v68/payments'
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
