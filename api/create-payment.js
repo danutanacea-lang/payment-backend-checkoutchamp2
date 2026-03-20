9648289fbf4dbb2011a6af41c481aa8f6336367c
danutanacea-lang <danuta.nacea@globalbrother.com>
2026-03-20 13:38:38 +0200

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -1,10 +1,6 @@
-import https from 'https';
-
 export default async function handler(req, res) {
   return res.status(200).json({
     apiKey: process.env.ADYEN_API_KEY || 'MISSING',
     merchant: process.env.ADYEN_MERCHANT_ACCOUNT || 'MISSING'
   });
-}    });
-  }
 }
